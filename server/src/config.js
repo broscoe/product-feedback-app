631c2164f4b3983d473f13802ee198923316e7c0
Bakari <bakari.roscoe@gmail.com>
2025-08-05 14:31:39 -0400

    Update config.js

--- server/src/config.js
+++ server/src/config.js
@@ -1,5 +1,1 @@
-const config = {
-  databaseUrl: "postgresql://neondb_owner:npg_fEewyX7pch1F@ep-sparkling-wind-aeiaezui-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
-};
 
-export default config;
